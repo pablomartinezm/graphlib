efb26c987a9a07315d7eb39836e8fcb463531463
monzo94 <monzo_94@hotmail.com>
2017-10-08 21:37:51 +0200

    Make code PEP8 compliant

--- classes/graph.py
+++ classes/graph.py
@@ -1,18 +1,18 @@
 __author__ = """\n""".join(['Sergio Montoya de Paco <sergio.mp1710@gmail.com>'])
 """
-Clase Graph, que representa un grafo (conjunto de vértices y aristas). La clase representa un grafo no dirigido, por lo tanto, 
+Clase Graph, que representa un grafo (conjunto de vértices y aristas). La clase representa un grafo no dirigido, por lo tanto,
 las aristas que unen dos vértices son bidireccionales.
 Métodos:
     -def __init__(self): Constructor de la clase, donde se crea el conjunto vacío de nodos y aristas.
-    -def node(self)(@property): Atributo que es un conjunto de nodos en un diccionario, donde la key es el nodo y el value son 
+    -def node(self)(@property): Atributo que es un conjunto de nodos en un diccionario, donde la key es el nodo y el value son
      los atributos del nodo.
-    -def edge(self)(@property): Atributo que es un conjunto de aristas en un diccionario, donde la key es un nodo y los value 
+    -def edge(self)(@property): Atributo que es un conjunto de aristas en un diccionario, donde la key es un nodo y los value
      son diccionarios, donde la key es el nodo al que va la arista y el value los atributos de dicha arista.
     -def nodes(self): Devuelve una lista con todos los nodos.
     -def edges(self): Devuelve una lista con tuplas que representan las aristas (nodo1,nodo2)
     -def add_node(self, node, attr_dict=None): Añade el nodo al diccionario de nodos, si el elemento ya existía, en caso de que
      se especifique un nuevo atributo, se añadirá al diccionario de atributos. 'attr_dict' = dictionary
-    -def add_edge(self, node1, node2, attr_dict=None): Añade una arista al diccionario de aristas, si uno de los nodos 
+    -def add_edge(self, node1, node2, attr_dict=None): Añade una arista al diccionario de aristas, si uno de los nodos
      especificados no existe, se creará el nodo de forma implícita. 'attr_dict' especifica un atributo para la arista, si ya
      contençia un atributo, se añade al diccionario de atributos.
     -def add_nodes_from(self, node_list, attr_dict=None): Añade todos los nodos especificados en la lista 'node_list', a todos
@@ -25,149 +25,143 @@ Métodos:
     -def __len__(self): Devuelve el número de nodos que hay en el grafo.
     -def neighbors(self, node): Devuelve una lista con los nodos incidentes en el nodo 'node'.
     -def remove_node(self, node1): Elimina el nodo 'node1' del grafo y todas sus aristas incidentes.
-    -def remove_edge(self, node1, node2): Elimina la arista (node1,node2) del grafo. Ímplicitamente se elimina también la 
+    -def remove_edge(self, node1, node2): Elimina la arista (node1,node2) del grafo. Ímplicitamente se elimina también la
      arista (node2,node1).
-    -def remove_nodes_from(self, node_list): Elimina todos los nodos especificados en la lista de nodos 'node_list'. De igual 
+    -def remove_nodes_from(self, node_list): Elimina todos los nodos especificados en la lista de nodos 'node_list'. De igual
      forma que en la función remove_node, se elimina el nodo y todas sus aristas incidentes.
     -def remove_edges_from(self, edge_list): Elimina todas las aristas especificadas en la lista de aristas 'edge_list'. De igual
      forma que en la función remove_edge, se elimina la arista birideccional (se elimina tanto (node1,node2) como (node2,node1)).
 """
 class Graph:
-    
+
     def __init__(self):
         self._nodes = {}
         self._edges = {}
-    
+
     @property
     def node(self):
         return self._nodes
-    
+
     @property
     def edge(self):
         return self._edges
-    
+
     def nodes(self):
         return list(self._nodes.keys())
-    
+
     def edges(self):
         edges_list = []
         for n1 in self._edges.keys():
             for n2 in self._edges[n1].keys():
-                if not (n2,n1) in edges_list:
-                    edges_list.append((n1,n2))
+                if not (n2, n1) in edges_list:
+                    edges_list.append((n1, n2))
         return edges_list
-    
+
     def add_node(self, node, attr_dict=None):
-        
+
         if attr_dict is None:
             node_dict = {}
         else:
-            #Tenemos que crear una copia del diccionario, porque si no utilitzamos el método add_nodes_from, estaremos
-            #asignando un mismo diccionario de atributos a múltiples nodos (y si modificamos los atributos de un nodo,
-            #modificaremos también los demás nodos).
+            # Tenemos que crear una copia del diccionario, porque si no utilitzamos el método add_nodes_from, estaremos
+            # asignando un mismo diccionario de atributos a múltiples nodos (y si modificamos los atributos de un nodo,
+            # modificaremos también los demás nodos).
             node_dict = attr_dict.copy()
-            
+
         if node in self._nodes:
             if attr_dict:
-                self._nodes[node].update(node_dict)    
+                self._nodes[node].update(node_dict)
         else:
             self._nodes[node] = node_dict
-            self._edges[node] = {} #Añadimos el nodo al diccionario de aristas, aunque su grado sea 0, así podremos
-                                   #utilizar métodos como degree(node) o neighbors(node).
+            # Añadimos el nodo al diccionario de aristas, aunque su grado sea 0, así podremos
+            # utilizar métodos como degree(node) o neighbors(node).
+            self._edges[node] = {}
 
-    
-    #Comprovamos si los dos nodos existe, si no existe alguno, lo creamos y después creamos la arista.
     def add_edge(self, node1, node2, attr_dict=None):
-        
+
         if attr_dict is None:
             edge_dict = {}
         else:
-            #Tenemos que crear una copia del diccionario, porque si no utilitzamos el método add_edges_from, estaremos
-            #asignando un mismo diccionario de atributos a múltiples aristas (y si modificamos los atributos de una arista,
-            #modificaremos también las demás aristas).
+            # Tenemos que crear una copia del diccionario, porque si no utilitzamos el método add_edges_from, estaremos
+            # asignando un mismo diccionario de atributos a múltiples aristas (y si modificamos los atributos de una arista,
+            # modificaremos también las demás aristas).
             edge_dict = attr_dict.copy()
-        if not node1 in self.nodes():
+
+        # Comprovamos si los dos nodos existe, si no existe alguno, lo creamos y después creamos la arista.
+        if node1 not in self.nodes():
             self.add_node(node1)
-        if not node2 in self.nodes():
+        if node2 not in self.nodes():
             self.add_node(node2)
-            
-        #Añadimos para una arista que va del nodo A al B, la arista que va de A a B y la arista que va de B a A.
-        for n1,n2 in [(node1,node2),(node2,node1)]:
-            
+
+        # Añadimos para una arista que va del nodo A al B, la arista que va de A a B y la arista que va de B a A.
+        for n1, n2 in [(node1, node2), (node2, node1)]:
+
             if self._edges[n1] != {}:
                 if n2 in self._edges[n1] and edge_dict:
                     self._edges[n1][n2].update(edge_dict)
                 else:
                     self._edges[n1][n2] = edge_dict
             else:
-                self._edges[n1].update({n2 : edge_dict})
-            
-                
-        
+                self._edges[n1].update({n2: edge_dict})
+
     def add_nodes_from(self, node_list, attr_dict=None):
         for new_node in node_list:
-            self.add_node(new_node,attr_dict)
-    
+            self.add_node(new_node, attr_dict)
+
     def add_edges_from(self, edge_list, attr_dict=None):
         for new_edge in edge_list:
-            self.add_edge(new_edge[0],new_edge[1],attr_dict)
+            self.add_edge(new_edge[0], new_edge[1], attr_dict)
 
-    def degree(self,node):
+    def degree(self, node):
         if node in self._edges:
             return len(self._edges[node])
         else:
             raise ValueError("El nodo " + str(node) + " no existe")
-    
+
     def __getitem__(self, node):
         if node in self._edges:
             return self._edges[node]
         else:
             raise ValueError("El nodo " + str(node) + " no existe")
-    
+
     def __len__(self):
         return len(self._nodes)
-    
+
     def neighbors(self, node):
         if node in self._edges:
             return list(self._edges[node].keys())
         else:
             raise ValueError("El nodo " + str(node) + " no existe")
-    
+
     def remove_node(self, node1):
-        if not node1 in self._nodes:
+        if node1 not in self._nodes:
             raise ValueError("El nodo " + str(node1) + " no existe")
-        
-        
-        #Primero eliminamos el nodo del conjunto de nodos
+
+        # Primero eliminamos el nodo del conjunto de nodos
         del self._nodes[node1]
-        
-        #Y ahora eliminamos todas sus aristas incidentes
+
+        # Y ahora eliminamos todas sus aristas incidentes
         if node1 in self._edges:
             edges_list_to_remove = [node2 for node2 in self._edges[node1].keys()]
             del self._edges[node1]
             for node in edges_list_to_remove:
                 del self._edges[node][node1]
-                
-                
-    
+
     def remove_edge(self, node1, node2):
-        if not node1 in self._edges:
+        if node1 not in self._edges:
             raise ValueError("El nodo " + str(node1) + " no existe")
-        if not node2 in self._edges:
+        if node2 not in self._edges:
             raise ValueError("El nodo " + str(node2) + " no existe")
-        if not node2 in self._edges[node1]:
+        if node2 not in self._edges[node1]:
             raise ValueError("El nodo " + str(node1) + " no está conectado con el nodo " + str(node2))
-        
-        #Eliminamos tanto la arista (node1,node2) como la arista (node2,node1)
-        for n1,n2 in [(node1,node2),(node2,node1)]:
+
+        # Eliminamos tanto la arista (node1,node2) como la arista (node2,node1)
+        for n1, n2 in [(node1, node2), (node2, node1)]:
             del self._edges[n1][n2]
-    
+
     def remove_nodes_from(self, node_list):
         for node in node_list:
             self.remove_node(node)
-    
+
     def remove_edges_from(self, edge_list):
         for edge in edge_list:
-            self.remove_edge(edge[0],edge[1])
-
-
+            self.remove_edge(edge[0], edge[1])
